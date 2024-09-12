58bd2f37b2f5e15453c7525440210d7a2a97ca04
Ubuntu <ubuntu@ip-172-26-6-143.ec2.internal>
2024-09-12 03:51:56 +0000

    2

--- pg.sh
+++ pg.sh
@@ -4,15 +4,15 @@
 sudo apt-get install -y postgresql postgresql-contrib
 
 # Load environment variables from config.env
-if [ -f config.env ]; then
-  export $(grep -v '^#' config.env | xargs)
+if [[ -f config.env ]]; then
+  source config.env
 else
   echo "config.env file not found!"
   exit 1
 fi
 
 # Check if environment variables are set
-if [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ]; then
+if [[ -z "$DB_USER" ]] || [[ -z "$DB_PASSWORD" ]] || [[ -z "$DB_NAME" ]]; then
   echo "Please set DB_USER, DB_PASSWORD, and DB_NAME environment variables."
   exit 1
 fi
@@ -22,13 +22,13 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 
 # Create the user with superuser privileges
-sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD' SUPERUSER;"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'DreamChainPgHHHJuIkLoHmNhYYuiNkjfaw';"
 
 # Create the database with the user as the owner
-sudo -u postgres psql -c "CREATE DATABASE ""$DB_NAME"";"
+sudo -u postgres psql -c "CREATE DATABASE \"$DB_NAME\" "
 
 # Create the schema owned by the new user
-sudo -u postgres psql  -d $DB_NAME -c "CREATE SCHEMA ""LICENSE"";"
+sudo -u postgres psql -d "$DB_NAME" -c "CREATE SCHEMA \"LICENSE\" ;"
 
 # Restart PostgreSQL service to apply changes
 sudo systemctl restart postgresql
@@ -36,3 +36,7 @@ sudo systemctl restart postgresql
 # Show PostgreSQL service status
 sudo systemctl status postgresql
 
+# Connect to the database using the created user
+psql -h localhost -U postgres -d "$DB_NAME" -W "$DB_PASSWORD"
+
+# Now you can execute SQL commands within the database
